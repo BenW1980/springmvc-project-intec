cf29ef6390fdff10e9ff90378463211b53d9808c
bw0202 <bw0202@gmail.com>
2015-02-19 12:23:25 +0100

    Fixed some formatting

--- src/main/java/be/intec/project/controllers/SourceCodeController.java
+++ src/main/java/be/intec/project/controllers/SourceCodeController.java
@@ -7,8 +7,6 @@ import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class SourceCodeController {
-	
-	
 
 	@RequestMapping(value = "/code", method = RequestMethod.GET)
 	public String sourceCode(Model model) {
